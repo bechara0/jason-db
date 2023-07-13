643014abd88f989d280f5553c0eb230e6c1aec03
bechara0 <jjbechara@gmail.come>
2023-07-13 07:45:54 -0300

    aca anda base de datos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    console.log("Effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promesa cumplida");
+      setNotes(response.data);
+    });
+  }, []);
+  console.log("render", notes.length, "notes");
+
+  return <div className="App">Datas Base</div>;
 }
 
 export default App;
